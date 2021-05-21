be22719f302ac82679abd98acb8cf572977c098e
safwanvk <safwanvalakundil@gmail.com>
2021-05-21 15:24:30 +0530

    :sparkles: Add banner ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
 import React from 'react'
-import NavBar from './Components/NavBar/NavBar';
+import NavBar from './Components/NavBar/NavBar'
+import './App.css'
+import Banner from './Components/Banner/Banner'
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+        <NavBar/>
+        <Banner/>
     </div>
   );
 }
