60f9c754db3ac2bf69e65573dbebe4a419f3fb17
safwanvk <safwanvalakundil@gmail.com>
2021-05-28 10:47:49 +0530

    :sparkles: Add integrate api in banner

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './Banner.css'
+import axios from '../../axios'
+import {API_KEY, imageUrl} from '../../utils/const'
 
 function Banner() {
+
+    const [movie, setMovie] = useState()
+
+    useEffect(() => {
+        axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+            setMovie(response.data.results[0])
+        })
+    }, [])
     return (
         <div>
-            <div className="banner">
+            <div className="banner" style={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path : ''})`}}>
                 <div className="content">
-                    <h1 className="title">Movie Name</h1>
+                    <h1 className="title">{movie ? movie.title : ''}</h1>
                     <div className="buttons">
                         <button className="button">Play</button>
                         <button className="button">My list</button>
                     </div>
-                    <h1 className="description">n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document </h1>
+                    <h1 className="description">{movie ? movie.overview : ''}</h1>
                 </div>
                 <div className="fade_bottom"></div>
             </div>
---
+++ src/utils/const.js
@@ -0,0 +1,3 @@
+export const baseUrl='https://api.themoviedb.org/3';
+export const API_KEY = '8db674f050efb6cc85d8a974651473a1';
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
