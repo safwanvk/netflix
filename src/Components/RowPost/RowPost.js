3fd74018e0fb9772129074d6172e72cdaffa7fcf
safwanvk <safwanvalakundil@gmail.com>
2021-05-28 12:32:38 +0530

    :sparkles: Add youtube integration for play trailer

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,10 +1,12 @@
 import React, {useState,useEffect} from 'react'
 import './RowPost.css'
 import axios from '../../axios';
-import {imageUrl} from '../../utils/const'
+import {API_KEY, imageUrl} from '../../utils/const'
+import YouTube from 'react-youtube';
 
 function RowPost(props) {
     const [movies, setMovies] = useState([])
+    const [urlId, setUrlId] = useState('')
     useEffect(() => {
         axios.get(props.url).then((response)=>{
             setMovies(response.data.results)
@@ -13,15 +15,31 @@ function RowPost(props) {
             alert(err)
         })
     }, [])
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+        },
+      };
+    const handleMovie = (id)=>{
+        axios.get(`movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then((response)=>{
+            if(response.data.results.length!==0){
+                setUrlId(response.data.results[0])
+            }
+        })
+    }
     return (
         <div className="row">
             <h2>{props.title}</h2>
             <div className="posters">
                 {movies.map((obj)=>
-                     <img className={props.isSmall ? "small_poster" : "poster"}
+                     <img onClick={()=>handleMovie(obj.id)} className={props.isSmall ? "small_poster" : "poster"}
                      src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
                 )}
             </div>
+            {urlId && <YouTube videoId={urlId.key} opts={opts}/>}
         </div>
     )
 }
