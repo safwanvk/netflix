528c5353e609a2f79c4c178d9adf9ea0e222590c
safwanvk <safwanvalakundil@gmail.com>
2021-05-28 12:41:47 +0530

    :zap: Add some genres list

--- src/utils/const.js
+++ src/utils/const.js
@@ -4,3 +4,6 @@ export const imageUrl = 'https://image.tmdb.org/t/p/original';
 
 export const originals = `discover/tv?api_key=${API_KEY}&with_networks=213`;
 export const action = `discover/movie?api_key=${API_KEY}&with_genres=28`
+export const comedy = `discover/movie?api_key=${API_KEY}&with_genres=35`
+export const horror = `discover/movie?api_key=${API_KEY}&with_genres=27`
+export const romance = `discover/movie?api_key=${API_KEY}&with_genres=10749`
